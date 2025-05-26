d8f4e69154ba6ecd9cbc07f10cc6d1b3dbae93a5
davidruncode <daguz.91@gmail.com>
2025-05-26 19:15:17 -0400

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/clerk-react";
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -25,11 +20,7 @@ function App() {
       <h1>Ant</h1>
       <div className="card">
         <header>
-          <SignedOut>
-            <SignInButton mode="modal"/>
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
+
           </SignedIn>
         </header>
         <p>
--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ClerkProvider } from '@clerk/clerk-react'
 
-//Import Clerk Publishable Key
-const PUBLICHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-if (!PUBLICHABLE_KEY) {
-  throw new Error('VITE_CLERK_PUBLISHABLE_KEY is not defined. Please set it in your .env file.')
-}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLICHABLE_KEY} afterSignOutUrl='/'>
       <App />
-    </ClerkProvider>
   </StrictMode>,
 )
